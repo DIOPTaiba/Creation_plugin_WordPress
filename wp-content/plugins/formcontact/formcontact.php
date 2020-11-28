9743249e0b5842218e027499f08d0ed16c0127a7
DIOPTaiba <mordioptaiba@gmail.com>
2020-11-28 09:39:00 +0000

    ajout plugin formulaire contact

---
+++ wp-content/plugins/formcontact/formcontact.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package formulaire contact mor
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Form Contact Simplon
+Plugin URI: https://simplon.co
+Description: Plugin word press pourr formulaire de contact
+Author: Mor DIOP Taiba
+Version: 1.0.0
+Author URI: https://simplon.co
+*/
+
